6f57017aa507179750e386ad45c11126a482bbf0
Priyanshu Dhariwal <priyanshudhariwal@gmail.com>
2023-10-24 14:53:20 +0530

    finished exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{}]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addName = (e) => {
     e.preventDefault()
@@ -23,7 +29,8 @@ const App = () => {
       e.preventDefault()
       const newPerson = {
         name: newName,
-        number: newNumber
+        number: newNumber,
+        id: persons.length + 1
       }
       setPersons(persons.concat(newPerson))
       setNewName('')
@@ -39,11 +46,21 @@ const App = () => {
     setNewNumber(e.target.value)
   }
 
+  const handleFilter = (e) => {
+    setFilter(e.target.value)
+  }
+
+  const personsToShow = (filter === null) ? persons : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()));
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter <input value={filter} onChange={handleFilter}/>
+      </div>
       <form onSubmit={addName}>
         <div>
+          <h1>Add New</h1>
           <div>name: <input value={newName} onChange={handleChange}/></div>
           <div>number: <input value={newNumber} onChange={handleNumChange}/></div>
         </div>
@@ -52,7 +69,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      {personsToShow.map(person => <p key={person.id}> {person.name} {person.number}</p>)}
     </div>
   )
 }
