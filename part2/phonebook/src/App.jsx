2834c9a7a02aaf5b9d949d482be4b58a505260d5
Priyanshu Dhariwal <priyanshudhariwal@gmail.com>
2023-11-08 23:23:19 +0530

    finished exercise 2.12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -41,9 +41,13 @@ const App = () => {
         number: newNumber,
         id: persons.length + 1
       }
-      setPersons(persons.concat(newPerson))
-      setNewName('')
-      setNewNumber('')
+      axios
+        .post('http://localhost:3001/persons', newPerson)
+        .then((response) => {
+          setPersons(persons.concat(response.data))
+          setNewName('')
+          setNewNumber('')
+        })
     }
   }
 
