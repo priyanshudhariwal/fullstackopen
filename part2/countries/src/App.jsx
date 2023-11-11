1dd22a81c2d23629493bd14c7c5331e03a00c100
Priyanshu Dhariwal <priyanshudhariwal@gmail.com>
2023-11-11 23:13:45 +0530

    filtering of countries accomplished, need to fix duplication in initial array

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,27 +1,51 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import Information from './components/Information'
 
 function App() {
   
+  let filteredCountries = []
   const countryNames = []
+  const [countries, setCountries] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
+  const [numberOfCountries, setNumberOfCountries] = useState(0)
+  const [filteredItems, setFilteredItems] = useState([])
 
-  const allCountries = () => {
-    axios
-      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+  const allCountries = async () => {
+    console.log('fetching')
+    await axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
       .then((response) => {
         response.data.forEach((dat) => {
           countryNames.push(dat.name.common)
         })
+        setCountries(countryNames)
       })
-      console.log(countryNames)
   }
 
-  useEffect(allCountries, [])
+
+  useEffect(() => {allCountries()}, [])
+
+  const handleFilter = (e) => {
+    let num = 0;
+    setSearchTerm(e.target.value)
+    countries.forEach((name) => {
+      if(name.toLowerCase().includes(searchTerm.toLowerCase())){
+        num++;
+        setNumberOfCountries(num)
+      }
+    })
+    filteredCountries = countries.filter((country) => country.toLowerCase().includes(searchTerm.toLowerCase()))
+    setFilteredItems(filteredCountries)
+  }
+
 
   return (
     <div>
-      Find countries <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+      Find countries <input value={searchTerm} onChange={handleFilter} />
+      <Information 
+        filteredList={filteredItems}
+        numberOfResults={numberOfCountries}
+      />
     </div>
   )
 }
--- part2/countries/src/components/Information.jsx
+++ part2/countries/src/components/Information.jsx
@@ -1,6 +1,23 @@
-const Information = () => {
+const Information = ({ filteredList, numberOfResults }) => {
+    console.log(filteredList)
+    if(numberOfResults > 10){
+        return(
+            <p>Too many matches, specify another filter</p>
+        )
+    }
+    else if(numberOfResults <= 10 && numberOfResults > 1){
+        return(
+            <div>
+                <ul>
+                    {filteredList.map((item) => <li>{item} </li>)}
+                </ul>
+            </div>
+        )
+    }
     return (
+        <div>
 
+        </div>
     )
 }
 
