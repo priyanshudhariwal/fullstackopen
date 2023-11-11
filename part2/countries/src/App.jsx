89bdc0ce12c0caa1bdbeff62262d70fbe46aa0a3
Priyanshu Dhariwal <priyanshudhariwal@gmail.com>
2023-11-11 22:00:13 +0530

    started app work

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,9 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+  
+  const countryNames = []
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const allCountries = () => {
+    axios
+      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then((response) => {
+        console.log(response.data)
+        console.log(response.data[0].name.common)
+        response.data.forEach((dat) => {
+          countryNames.concat(dat.name.common)
+        })
+      })
+      console.log(countryNames)
+  }
+
+  useEffect(allCountries, [])
+
   return (
     <div>
-      <p>Hello from App!</p>
+      Find countries <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
     </div>
   )
 }
